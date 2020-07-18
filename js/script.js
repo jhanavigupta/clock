94d1ee27fa155fa179a64ee9eac34fae3129283f
jhanavigupta <jhanavigupta3936@gmail.com>
2020-07-19 01:45:39 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@ function setClock() {
     const currentDate = new Date();
 
     const secondRatio = currentDate.getSeconds() / 60;
-    const minuteRatio = (secondRatio + currentDate.getHours()) / 60;
-    const hourRatio = (minuteRatio + currentDate.getMinutes()) / 12;
+    const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
+    const hourRatio = (minuteRatio + currentDate.getHours()) / 12;
+
     setRotation(secondHand, secondRatio);
     setRotation(minuteHand, minuteRatio);
     setRotation(hourHand, hourRatio);
